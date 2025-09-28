ef88517f143b6aabddb3832a0125fc305954bd2e
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-09-28 22:53:44 +0600

    Practice with print function in python.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-practice
 Deep drive into python language
+
+# Introduction
+Python was created by Guido van Rossum and released in 1991
+
+
--- basic/1print.py
+++ basic/1print.py
@@ -1,3 +1,28 @@
 
-#print the string by print function
-print("Jacky")
+#Multiple comment: print function
+
+""" print("Hello, World!")
+print("Welcome", "to", "Jacky's Dream world!")
+print()
+print("Hope you are enjoying python programming.") """
+
+#Single comment: print function
+""" print("Hello, World!")
+print("Welcome", "to", "Jacky's Dream World!")
+print()
+print("Hope you are enjoying python programming.") """
+
+
+
+#More examples of print function
+#end parameter and sep parameter
+
+#end parameter takes a string value and appends it at the end of the output
+print("Welcome", end="   ")
+print("to jacky's Dream World!")
+
+#sep parameter takes a string value and separates the values with it
+print("28", "09", "2025", sep="-")
+print("28", "09", "2025", sep="-", end="   ")
+print("Happy Birthday to me!")
+
