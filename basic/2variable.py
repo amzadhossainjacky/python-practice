b4b7aecbb74d8da285764875c91fe78b4166e003
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-09-29 22:41:07 +0600

    Practice variables in python

---
+++ basic/2variable.py
@@ -0,0 +1,45 @@
+
+#variable
+""" x = 10
+y = 20
+print(x, y) """
+
+#variable with arithmetic operation
+""" x = 34
+y = 45
+z = x + y
+print(z) """
+
+#multiple variable
+""" x, y = 10, 40
+print(x, y) """
+
+#type checking of variable
+""" x = 10
+y = "Jacky"
+print(10, "type of x is:", type(x))
+print(y, "type of y is:",type(y)) """
+
+#variable swapping
+""" x = 10
+y = 20      
+x, y = y, x
+print("After swapping: x =", x, "y =", y)
+ """
+
+#type casting of variables
+""" x = 10
+y = "20"
+print("Before type casting: x =", x, "type of x is:", type(x))
+print("Before type casting: y =", y, "type of y is:", type(y))
+k = str(x)
+j = int(y)
+print("After type casting: k = ", k, "And type of j is:", type(k))
+print("After type casting: j = ", j, "And type of j is:", type(j)) """
+
+#unpack a collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
