e2fe179ff5ac717c36cff556e337fa40cfca82d3
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-10-05 22:19:25 +0600

    practice on data type

---
+++ basic/3datatype.py
@@ -0,0 +1,59 @@
+#type function uses for data type checking
+
+""" x = 5
+print('The data type of x is:', type(x))
+name = "Jacky"
+print("TThe type of name is:", type(name)) """
+
+#Different data types in python
+#int, float, str, bool, list, tuple, set, dict
+
+#int data type
+""" num = 23
+print(num , "is of type:", type(num)) """
+
+#float data type
+""" float_num = 23.45
+print(float_num, "is of type:", type(float_num)) """
+
+#complex data type
+""" complex_num = 2 + 3j
+print(complex_num, "is of type:", type(complex_num))  """
+
+
+#string data type 
+""" book_name = "Python Programming"
+print(book_name, "is of type:", type(book_name)) """
+
+#boolean data type
+""" a = True
+b = False 
+print("a is of type:", type(a), "and b is of type:", type(b))
+ """
+
+#list data type
+""" fruits = ["apple, mango, banana"]
+print("fruits is of type:", type(fruits)) """
+
+#tuple data type
+""" skills = ("python", "php", "javascript")
+print("skills is of type:", type(skills)) """
+
+#set data type
+""" unique_numbers = {1, 2, 3, 4, 5}
+print("unique numbers is of type:", type(unique_numbers)) """
+
+#dictionary data type
+""" student = {
+    "name": "Md. Amzad Hossain Jacky",
+    "age": 29,
+    "city": "Dhaka",
+    "is_student": False
+}
+
+print("student is of type:", type(student)) """
+
+#range data type
+""" numbers = range(1, 10)
+print("numbers is of type:", type(numbers)) """
+
